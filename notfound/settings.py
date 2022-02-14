0f6b8ee30c0ba2f8fe44045124a8e65748f1bb1f
lee <higgs95@naver.com>
2022-02-14 17:56:19 +0900

    Add: initial settings

--- notfound/settings.py
+++ notfound/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from my_settings import DATABASES,SECRET_KEY
+
+from my_settings import DATABASES, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
