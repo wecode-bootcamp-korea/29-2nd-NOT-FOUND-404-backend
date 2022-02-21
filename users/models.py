0bf3ba86b56779cd6fbe9b6fa26698c61469f053
jiyeon.kim.ds <jiyeon.kim.ds@gmail.com>
2022-02-21 18:48:33 +0900

    [ADD]add on_delete to User model
    [MODIFY] modify user modeling

--- users/models.py
+++ users/models.py
@@ -4,11 +4,16 @@ from django.db import models
 
 from core.models import TimeStampedModel
 
+class UserRankEnum(Enum):
+    AMATEUR      = 1
+    PROFESSIONAL = 2
+    MASTER       = 3
+
 class User(TimeStampedModel):
     name     = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
     kakao_id = models.IntegerField(unique=True)
-    rank     = models.ForeignKey('UserRank')
+    rank     = models.ForeignKey('UserRank', on_delete=models.SET_DEFAULT, default=UserRankEnum.AMATEUR.value)
     point    = models.IntegerField(default=100000)
 
     class Meta:
@@ -17,7 +22,5 @@ class User(TimeStampedModel):
 class UserRank(models.Model):
     rank = models.CharField(max_length=20)
 
-class UserRankEnum(Enum):
-    AMATEUR      = 1
-    PROFESSIONAL = 2
-    MASTER       = 3
+    class Meta:
+        db_table = 'users_ranks'
